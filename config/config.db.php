e93df7cdb5cae7c1225f6bb7d8fc5e7cc9301d2b
Maggi <yuen.maggi@gmail.com>
2025-07-07 09:32:52 +0200

    lib.php with INSERT

--- config/config.db.php
+++ config/config.db.php
@@ -2,7 +2,7 @@
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$mysqli = new mysqli('db', 'root', 'test', 'erinnerungsDB');
+$mysqli = new mysqli('db', 'root', 'test', 'api-test_db');
 
 if($mysqli->connect_errno){
     throw new RuntimeException('mysqli-Verbindungsfehler: '. $mysqli->connect_error);
---
+++ config/lib.php
@@ -0,0 +1,20 @@
+<?php
+
+function register($name, $tel, $pwd, $description){
+    global $mysqli;
+
+    $sql = "INSERT INTO registrations SET name=?, tel=?, pwd=?,description=?; ";
+    $stmt = $mysqli->prepare($sql);
+    if (!$stmt){
+        throw new Exception($mysqli->error);
+    }
+    $stmt->bind_param('siss', $name, $tel, $pwd, $description);
+    if (!$stmt->execute()){
+        throw new Exception($$stmt->error);
+    }
+    return $stmt->affected_rows;
+}
+
+
+
+?>
