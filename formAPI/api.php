200c78badcb53bb8b18d734c8f94878cafc67798
Maggi <yuen.maggi@gmail.com>
2025-07-07 13:15:15 +0200

    api

---
+++ formAPI/api.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+$response = [];
+
+if (empty($_POST['frm_tel'])) {
+    $response = [
+        'status' => 'error',
+        'message' => 'Keine Telefonnumer angegeben.'
+    ];
+    http_response_code(400);
+} else {
+    $response = [
+        'status' => 'ok',
+        'message' => [
+            "Erfolg" => 'Daten erfolgreich gespeichert.',
+            'data' => $_POST['frm_tel']
+        ]
+    ];
+    http_response_code(200);
+}
+
+echo json_encode($response);
