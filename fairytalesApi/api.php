6a77acd996a264f7fb459b386f6d68eaa7ee9872
Maggi <yuen.maggi@gmail.com>
2025-07-09 09:15:38 +0200

    fairytales

---
+++ fairytalesApi/api.php
@@ -0,0 +1,14 @@
+<?php
+
+$fairytales = ['Schneewittchen', 
+'Hänsel und Gretel', 
+'Die kleine Meerjungfrau', 
+'Der Froschkönig oder der eiserne Heinrich', 
+'Brüderchen und Schwesterchen', 
+'Das tapfere Schneiderlein',
+'Aschenputtel',
+'Die Bremer Stadtmusikanten',
+'König Drosselbart',
+'Rumpelstilzchen'];
+
+?>
