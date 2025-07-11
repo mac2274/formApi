b6e884bb23da9d6225f2f3a662a9b6be6fa6775d
Maggi <yuen.maggi@gmail.com>
2025-07-11 11:33:00 +0200

    api ausgetauscht

--- formAPI/alt/api.php
+++ formAPI/alt/api.php
@@ -1,17 +1,16 @@
 <?php
+// header('Content-Type: application/x-www-form-urlencoded'); JETZT ist im js ein accept
 require_once 'config/config.db.php';
-
 global $mysqli;
+
 $sql = "INSERT INTO registrations SET name=?, tel=?, pwd=?, description=? ";
 $stmt = $mysqli->prepare($sql);
 if (!$stmt) {
     throw new Exception($mysqli->error);
 }
 $stmt->bind_param('siss', $_POST['frm_name'], $_POST['frm_tel'], $_POST['frm_pwd'], $_POST['frm_descript']);
-if (!$stmt->execute()){
+if (!$stmt->execute()) {
     throw new Exception($stmt->error);
 }
-
 return $stmt->affected_rows;
 
-?>
