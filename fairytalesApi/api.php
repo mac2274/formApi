7173c7af5af9194f8fb1f7a3f1cb135b23804039
Maggi <yuen.maggi@gmail.com>
2025-07-11 08:38:06 +0200

    commit

--- fairytalesApi/api.php
+++ fairytalesApi/api.php
@@ -1,14 +1,32 @@
 <?php
+header('Content-Type: application/json');
 
-$fairytales = ['Schneewittchen', 
-'Hänsel und Gretel', 
-'Die kleine Meerjungfrau', 
-'Der Froschkönig oder der eiserne Heinrich', 
-'Brüderchen und Schwesterchen', 
-'Das tapfere Schneiderlein',
-'Aschenputtel',
-'Die Bremer Stadtmusikanten',
-'König Drosselbart',
-'Rumpelstilzchen'];
+$fairytales = [
+    ['story' => 'Schneewittchen'],
+    ['story' => 'Hänsel und Gretel'],
+    ['story' => 'Die kleine Meerjungfrau'],
+    ['story' => 'Der Froschkönig oder der eiserne Heinrich'],
+    ['story' => 'Brüderchen und Schwesterchen'],
+    ['story' => 'Das tapfere Schneiderlein'],
+    ['story' => 'Aschenputtel'],
+    ['story' => 'Die Bremer Stadtmusikanten'],
+    ['story' => 'König Drosselbart'],
+    ['story' => 'Rumpelstilzchen'],
+    ['story' => 'Schöne und das Biest']
+];
+
+echo json_encode($fairytales);
+
+// Prüfen, ob Parameter übergeben wurde
+if (isset($_GET['story'])){
+    $filter = 'und';
+    $search = str_contains($tale, $searchFor);
+}
+
+// if (isset(($_GET['story'])) === 'Aschenputtel'){
+//     echo 'Du hast einen Treffer.';
+// } else {
+//     echo 'Leider keinen Treffer.';
+// }
 
 ?>
--- formAPI/alt/api.php
+++ formAPI/alt/api.php
@@ -1,16 +1,17 @@
 <?php
-
 require_once 'config/config.db.php';
-global $mysqli;
 
+global $mysqli;
 $sql = "INSERT INTO registrations SET name=?, tel=?, pwd=?, description=? ";
 $stmt = $mysqli->prepare($sql);
 if (!$stmt) {
     throw new Exception($mysqli->error);
 }
 $stmt->bind_param('siss', $_POST['frm_name'], $_POST['frm_tel'], $_POST['frm_pwd'], $_POST['frm_descript']);
-if (!$stmt->execute()) {
+if (!$stmt->execute()){
     throw new Exception($stmt->error);
 }
+
 return $stmt->affected_rows;
 
+?>
